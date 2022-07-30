184412aef4b59b5f51a7f038449d2dffb76db609
Pauwul's macbook <petrila.paulcosmin@gmail.com>
2022-07-30 16:34:39 +0300

    First tries with a MacOS environment, I intend to move to Windows eventually

---
+++ Tema1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tema1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = args[0];
+            Console.WriteLine("Hello World!"+myName);
+        }
+
+
+    }
+
+   
+}   
